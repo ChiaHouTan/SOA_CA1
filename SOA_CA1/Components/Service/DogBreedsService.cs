c69bf9801bcc3b5d280bc41240e2e0b82374ebef
ChiaHouTan <57621046+ChiaHouTan@users.noreply.github.com>
2024-10-15 12:41:21 +0100

    git link

---
+++ SOA_CA1/Components/Service/DogBreedsService.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace SOA_CA1.Components.Service
+{
+    public class DogBreedsService
+    {
+        static readonly string Sanitize_url = "https://api.api-ninjas.com/v1/dogs";
+
+        public string SanitizeText(string rawvalue)
+        {
+            var client = new RestClient(Sanitize_url);
+            var request = new RestRequest();
+            request.AddParameter("X-Api-Key", "kEYxvRU7WUwd77Gdv6zrRQ==iK3TgdqZnMER5aXu");
+            var response = client.Execute(request);
+
+
+            return string.Empty;
+
+        }
+
+        
+    }
+
+    
+}
