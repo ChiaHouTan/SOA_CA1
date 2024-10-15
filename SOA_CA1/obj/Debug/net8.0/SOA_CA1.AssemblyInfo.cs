196ec57101a7a4f10bfaba5f888263f8aa011bff
ChiaHouTan <57621046+ChiaHouTan@users.noreply.github.com>
2024-10-15 18:56:29 +0100

    add compare razor page that compare 2 breed type

--- SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
+++ SOA_CA1/obj/Debug/net8.0/SOA_CA1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8666d5a18dc353413b2358e445cb45672db14168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c19eccd148556f7c2fe98739b9f0cc08ce3b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SOA_CA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
